2ad0601e5b1cd0cb3f348f76b9f12122c9fc327f
xlucekv <kontosocialeglowne@gmail.com>
2026-08-14 20:54:04 +0200

    Update botConfig.js

--- src/config/botConfig.js
+++ src/config/botConfig.js
@@ -1,5 +1,8 @@
+export function getColor() {
+    return "#5865F25"; // lub zwracaj dowolny kolor lub hex, którego używasz
+}
+
 export default {
-    // Podstawowa konfiguracja bota
     botName: "TitanBot",
     embedColor: "#5865F2",
 };
