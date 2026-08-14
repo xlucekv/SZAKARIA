053836e57725eb40de27e21a8b7395cc23b72199
xlucekv <kontosocialeglowne@gmail.com>
2026-08-14 03:14:16 +0200

    Update collectionService.js

--- src/services/collectionService.js
+++ src/services/collectionService.js
@@ -1,1 +1,17 @@
+const activeCollections = new Map();
 
+export async function getCollection(messageId) {
+    return activeCollections.get(messageId);
+}
+
+export async function saveCollection(messageId, data) {
+    activeCollections.set(messageId, data);
+}
+
+export async function updateDeposit(messageId, userId, amount) {
+    const collection = activeCollections.get(messageId);
+    if (!collection) return null;
+
+    collection.deposits[userId] = (collection.deposits[userId] || 0) + amount;
+    return collection;
+}
