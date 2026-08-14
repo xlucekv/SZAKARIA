bb8f6ff7caf942c01fc3a1b8fe92a9eb3a864bef
xlucekv <kontosocialeglowne@gmail.com>
2026-08-14 20:48:49 +0200

    Update tickets.js

--- src/commands/tickets.js
+++ src/commands/tickets.js
@@ -1,1 +1,16 @@
+import { SlashCommandBuilder } from 'discord.js';
 
+export default {
+    data: new SlashCommandBuilder()
+        .setName('ticket')
+        .setDescription('Tworzy panel zarządzania biletami'),
+    
+    category: 'Utility',
+
+    async execute(interaction) {
+        await interaction.reply({
+            content: '> 🎫 | Panel biletów jest w budowie!',
+            ephemeral: true,
+        });
+    },
+};
